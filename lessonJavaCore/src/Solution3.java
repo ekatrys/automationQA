07752afb2804592b525d237cf7304188aea7491c
basova <basova@tutu.ru>
2018-07-14 17:03:48 +0300

    add home work

---
+++ lessonJavaCore/src/Solution3.java
@@ -0,0 +1,9 @@
+public class Solution3 {
+
+    public static void main(String[] arg) {
+        double c = 2.0;
+        byte n = 50;
+        long a = 200;
+        int result = (int)((a / n) * c);
+    }
+}
